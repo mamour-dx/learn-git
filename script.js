e20111a6ee275a720133304122e77716dae0ae48
sow-abdou <abdourahmane.sow@talentsconsult.org>
2024-11-23 12:28:59 +0000

    improve design :lipstick:

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+window.addEventListener('scroll', () => {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.querySelector('.progress-bar').style.width = scrolled + '%';
+});
 
-  
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+});
+
+document.querySelectorAll('.reveal-on-scroll').forEach((element) => {
+    observer.observe(element);
+});
